a83ceb819d5e1c4d598737790fe658c319af373e
Jhosepe187 <jhosepbazanv@gmail.com>
2026-04-29 20:03:13 -0500

    Update (atributos y constructor)

--- src/Trabajador.java
+++ src/Trabajador.java
@@ -8,5 +8,18 @@
  * @author UCC20306
  */
 public class Trabajador {
+    private String nombre;
+    private String tipoDoc;
+    private String numDoc;
+    private String pension;
+    private double sueldoBase;
     
+    // Constructor
+    public Trabajador(String nombre, String tipoDoc, String numDoc, String pension, double sueldoBase) {
+        this.nombre = nombre;
+        this.tipoDoc = tipoDoc;
+        this.numDoc = numDoc;
+        this.pension = pension;
+        this.sueldoBase = sueldoBase;
+    }
 }
