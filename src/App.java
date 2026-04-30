652c9dd5f0b7f76036bc8e0736912eec29ce9b14
Jhosepe187 <jhosepbazanv@gmail.com>
2026-04-29 20:32:02 -0500

    Update (Agregamos clase APP la main y instanciamos scanner y array, añadimos un menu de opciones)

---
+++ src/App.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+import java.util.ArrayList;
+import java.util.Scanner;
+/**
+ *
+ * @author UCC20306
+ */
+public class App {
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Trabajador> lista = new ArrayList<>();
+        
+        int opcion = 0;
+
+        do{
+            try {
+                System.out.println("\nINNOVADORES S.A.");
+                System.out.println("1. Registrar nuevo trabajador");
+                System.out.println("2. Listar planilla y cálculos");
+                System.out.println("3. Salir");
+                System.out.print("Seleccione una opción: ");
+            }
+        }
+       
+    }  
